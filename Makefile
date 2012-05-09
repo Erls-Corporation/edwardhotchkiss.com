b102474add3bef8e9b4f365a90581f349e073e01
Edward Hotchkiss <edwardhotchkiss@me.com>
2012-05-09 14:18:37 -0400

    [mixpanel]

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 
 CSS_DIR = assets/css/
-
 LESS_DIR = assets/less/
 
 css:
 	lessc ${LESS_DIR}index.less ${CSS_DIR}main.min.css -compress
+
+JS_DIR = assets/js/
+
+js:
+	uglifyjs -o ${JS_DIR}main.min.js --no-mangle --no-squeeze ${JS_DIR}jekyll.js
 
 run:
 	jekyll --server
---
+++ assets/js/jekyll.js
@@ -0,0 +1,18 @@
+
+/**
+ * Jekyll
+ */
+
+var jekyll = {};
+
+jekyll.mpq = {
+  track : function(eventName, properties, callback) {
+    properties = properties || {};
+    callback = callback || function(){};
+    if (!/localhost/.test(document.location.hostname)) {
+      mpq.track(eventName, properties, callback);
+    };
+  }
+};
+
+ /* EOF */
